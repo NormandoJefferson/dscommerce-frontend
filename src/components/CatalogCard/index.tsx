a3cae8f86c3cd8582795f9f54e5197b70756610e
norma <normandojefferson@hotmail.com>
2024-06-18 11:18:58 -0300

    Update project: Searchbar onSearch event

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,9 +1,9 @@
 import "./styles.css";
-import { productDTO } from "../../models/product";
+import { ProductDTO } from "../../models/product";
 import { Link } from "react-router-dom";
 
 type Props = {
-  product: productDTO;
+  product: ProductDTO;
 };
 
 export default function CatalogCard({ product }: Props) {
@@ -15,7 +15,7 @@ export default function CatalogCard({ product }: Props) {
         </div>
         <div className="dsc-catalog-card-bottom">
           <h3>{product.price.toFixed(2)}</h3>
-          <h4>{product.description}</h4>
+          <h4>{product.name}</h4>
         </div>
       </div>
     </Link>
--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -3,22 +3,27 @@ import SearchBar from "../../../components/SearchBar";
 import CatalogCard from "../../../components/CatalogCard";
 import ButtonNextPage from "../../../components/ButtonNextPage";
 import { useEffect, useState } from "react";
-import { productDTO } from "../../../models/product";
+import { ProductDTO } from "../../../models/product";
 import * as productService from "../../../services/product-service";
 
 export default function Catalog() {
-  const [products, setProducts] = useState<productDTO[]>([]);
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+  const [productName, setProductName] = useState("");
 
   useEffect(() => {
-    productService.findAll().then((response) => {
+    productService.findPageRequest(0, productName).then((response) => {
       setProducts(response.data.content);
     });
-  }, []);
+  }, [productName]);
+
+  function handleSearch(searchText: string) {
+    setProductName(searchText);
+  }
 
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} />
 
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {products.map((product) => (
