68f7dd59ea8d6a666330228481283223437802ad
norma <normandojefferson@hotmail.com>
2024-11-02 21:07:35 -0300

    feat: update and post form

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import './styles.css';
 import FormInput from '../../../components/FormInput';
 import * as forms from "../../../utils/forms";
@@ -13,6 +13,8 @@ import { selectStyles } from "../../../utils/select";
 export default function ProductForm() {
 
   const params = useParams();
+
+  const navigate = useNavigate();
 
   const isEditing = params.productId !== 'create';
 
@@ -101,7 +103,17 @@ export default function ProductForm() {
       setFormData(formDataValidated);
       return;
     }
-    // console.log(forms.toValues(formData));
+    const requestBody = (forms.toValues(formData));
+    if (isEditing) {
+      requestBody.id = params.productId;
+    }
+    const request = isEditing
+      ? productService.updateRequest(requestBody)
+      : productService.insertRequest(requestBody);
+    request
+      .then(() => {
+        navigate("/admin/products");
+      });
   }
 
   return (
--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -1,7 +1,8 @@
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
+import { ProductDTO } from "../models/product";
 
-export function findPageRequest( page: number, name: string, size = 12, sort = "name") {
+export function findPageRequest(page: number, name: string, size = 12, sort = "name") {
   const config: AxiosRequestConfig = {
     method: "GET",
     url: "/products",
@@ -24,6 +25,26 @@ export function deleteById(id: number) {
     method: "DELETE",
     url: `/products/${id}`,
     withCredentials: true
+  }
+  return requestBackend(config);
+}
+
+export function updateRequest(obj: ProductDTO) {
+  const config: AxiosRequestConfig = {
+    method: "PUT",
+    url: `/products/${obj.id}`,
+    withCredentials: true,
+    data: obj
+  }
+  return requestBackend(config);
+}
+
+export function insertRequest(obj: ProductDTO) {
+  const config: AxiosRequestConfig = {
+    method: "POST",
+    url: "/products",
+    withCredentials: true,
+    data: obj
   }
   return requestBackend(config);
 }
