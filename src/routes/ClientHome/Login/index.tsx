161999cde4867d294e7ce05e9dd7f38c1657b592
norma <normandojefferson@hotmail.com>
2024-11-03 09:21:12 -0300

    feat: Login form global message

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,16 +1,17 @@
-import { useContext, useState } from "react";
 import "./styles.css";
+import { useContext, useState } from "react";
+import * as forms from '../../../utils/forms';
 import * as authService from "../../../services/auth-services";
 import { useNavigate } from "react-router-dom";
 import { ContextToken } from "../../../utils/context-token";
 import FormInput from "../../../components/FormInput";
-import * as forms from "../../../utils/forms";
 
 export default function Login() {
-
   const { setContextTokenPayload } = useContext(ContextToken);
 
   const navigate = useNavigate();
+
+  const [submitResponseFail, setSubmitResponseFail] = useState(false);
 
   const [formData, setFormData] = useState<any>({
     username: {
@@ -20,7 +21,9 @@ export default function Login() {
       type: "text",
       placeholder: "Email",
       validation: function (value: string) {
-        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value.toLowerCase());
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          value.toLowerCase()
+        );
       },
       message: "Favor informar um email válido",
     },
@@ -30,41 +33,45 @@ export default function Login() {
       name: "password",
       type: "password",
       placeholder: "Senha",
-    }
+    },
   });
 
   function handleSubmit(event: any) {
     event.preventDefault();
+
+    setSubmitResponseFail(false);
+
+    const formDataValidated = forms.dirtyAndValidateAll(formData);
+    if (forms.hasAnyInvalid(formDataValidated)) {
+      setFormData(formDataValidated);
+      return;
+    }
+
     authService
       .loginRequest(forms.toValues(formData))
       .then((response) => {
         authService.saveAccessToken(response.data.access_token);
         setContextTokenPayload(authService.getAccessTokenPayload());
-        navigate("/cart")
+        navigate("/cart");
       })
-      .catch((error) => {
-        console.log("Erro no login", error);
+      .catch(() => {
+        setSubmitResponseFail(true);
       });
   }
 
   function handleInputChange(event: any) {
-    const result = forms.updateAndValidate(formData, event.target.name, event.target.value);
-    setFormData(result);
+    setFormData(forms.updateAndValidate(formData, event.target.name, event.target.value));
   }
-
   function handleTurnDirty(name: string) {
-    const newFormData = forms.dirtyAndValidate(formData, name);
-    setFormData(newFormData);
+    setFormData(forms.dirtyAndValidate(formData, name));
   }
 
   return (
     <main>
-      <section id="login-section" className="dsc-container" onSubmit={handleSubmit}>
+      <section id="login-section" className="dsc-container">
         <div className="dsc-login-form-container">
-
-          <form className="dsc-card dsc-form">
+          <form className="dsc-card dsc-form" onSubmit={handleSubmit}>
             <h2>Login</h2>
-
             <div className="dsc-form-controls-container">
               <div>
                 <FormInput
@@ -73,10 +80,9 @@ export default function Login() {
                   onTurnDirty={handleTurnDirty}
                   onChange={handleInputChange}
                 />
-
-                <div className="dsc-form-error"></div>
+                <div className="dsc-form-error">{formData.username.message}</div>
               </div>
-
+              
               <div>
                 <FormInput
                   {...formData.password}
@@ -86,6 +92,13 @@ export default function Login() {
                 />
               </div>
             </div>
+
+            {
+              submitResponseFail &&
+              <div className="dsc-form-global-error">
+                Usuário ou senha inválidos
+              </div>
+            }
 
             <div className="dsc-login-form-buttons dsc-mt20">
               <button type="submit" className="dsc-btn dsc-btn-blue">
